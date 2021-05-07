be7ca14923e913a8bff7b0d030d5e810e13cee05
Ashoka Devara <ashoka.devara@capgemini.com>
2021-05-07 12:23:03 +0530

    second commit

---
+++ src/main/java/com/adsoft/adsoftbackend/AdsoftBackendApplication.java
@@ -0,0 +1,13 @@
+package com.adsoft.adsoftbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdsoftBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdsoftBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adsoft/adsoftbackend/AdsoftBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adsoft.adsoftbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdsoftBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
