9efba02eefeff2bd40afc7bf8b57be8ae6b0c7f0
Ashoka Devara <ashoka.devara@capgemini.com>
2021-05-07 13:08:26 +0530

    User class add++

---
+++ src/main/java/com/adsoft/model/User.java
@@ -0,0 +1,67 @@
+package com.adsoft.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	@Column(name="first_name")
+	private String firstname;
+	@Column(name="last_name")
+	private String lastName;
+	@Column(name="email")
+	private String email;
+
+	public User() {
+
+	}
+
+	public User(long id, String firstname, String lastName, String email) {
+		super();
+		this.id = id;
+		this.firstname = firstname;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
